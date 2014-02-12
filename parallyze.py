addb8c3ab9f8999f83c359fea59a074eda30e498
Elizabeth Baird <eliz.baird@gmail.com>
2014-02-12 17:39:19 -0500

    continuation of file incorporation and validation

--- parallyze.py
+++ parallyze.py
@@ -2,22 +2,30 @@
 
 #usage: python parallyze.py config.txt [genomediff1.gd genomediff2.gd ... n : referencegenome.gb(k) {genbank}]
 
-import argparse
 #find config.txt
 #find .gd files, save in list
 #try to break
 
+import argparse
+
 def main():
 	parser = argparse.ArgumentParser()
+#	parser.parse_args()
+	parser.add_argument(dest='config', help="First entry should be a config.txt file. File should be in working folder.")
 	parser.add_argument('-r', dest='reference', default="NONE")
-	parser.add_argument(dest='config')
 	gdfiles=[]
-        for i in cmd:
-                if i.endswith(".gd")
-                        gdfiles.append(i)
+      #  for i in cmd/argument/args:
+       #         if i.endswith(".gd"):
+        #                gdfiles.append(i)
 	args = parser.parse_args()
 	
-	print "Hello World!"
+	print "\nHello World!\n"
+	if args.config != 'config.txt':
+		print "Error: please ensure file is titled config.txt"
+#	elif 'config'!=a text file in right format
+#		print "Error: please ensure file is a .txt file"
+#	elif 'config' != contents of file properly formatted/layed out
+#		print "Error: please ensure contents of file are appropriate"
 	if args.reference:
 		print "The reference genome is", args.reference
 main()
