25eeaebe6b11872c0e4c9c02fa5a4cb1f981a610
Elizabeth Baird <eliz.baird@gmail.com>
2014-02-12 11:12:55 -0500

    add/modify input data from user

--- parallyze.py
+++ parallyze.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
 
+#usage: python parallyze.py config.txt [genomediff1.gd genomediff2.gd ... n : referencegenome.gb(k) {genbank}]
 
-#ejbaird
+import argparse
+#find config.txt
+#find .gd files, save in list
+#try to break
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-r', dest='reference', default="NONE")
+	parser.add_argument(dest='config')
+	gdfiles=[]
+        for i in cmd:
+                if i.endswith(".gd")
+                        gdfiles.append(i)
+	args = parser.parse_args()
+	
+	print "Hello World!"
+	if args.reference:
+		print "The reference genome is", args.reference
+main()
