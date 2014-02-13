fefd8b23e4c48b0b957f5b80453e704d5885a5e2
Elizabeth Baird <eliz.baird@gmail.com>
2014-02-13 14:51:31 -0500

    continuing work of defining and testing

--- parallyze.py
+++ parallyze.py
@@ -13,19 +13,20 @@ def main():
 #	parser.parse_args()
 	parser.add_argument(dest='config', help="First entry should be a config.txt file. File should be in working folder.")
 	parser.add_argument('-r', dest='reference', default="NONE")
-	gdfiles=[]
-      #  for i in cmd/argument/args:
-       #         if i.endswith(".gd"):
-        #                gdfiles.append(i)
+#	args.gdfiles=[]
+ #       for i in args:
+  #              if i.endswith(".gd"):
+   #                     args.gdfiles.append(i)
 	args = parser.parse_args()
 	
 	print "\nHello World!\n"
 	if args.config != 'config.txt':
 		print "Error: please ensure file is titled config.txt"
-#	elif 'config'!=a text file in right format
+#	elif args.config != a text file in right format
 #		print "Error: please ensure file is a .txt file"
-#	elif 'config' != contents of file properly formatted/layed out
+#	elif args.config != contents of file properly formatted/layed out
 #		print "Error: please ensure contents of file are appropriate"
 	if args.reference:
 		print "The reference genome is", args.reference
+	print args
 main()
