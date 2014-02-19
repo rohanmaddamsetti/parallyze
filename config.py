726bc2a3426aa3d40469ee20c00a79b136a63c2a
Elizabeth Baird <eliz.baird@gmail.com>
2014-02-19 11:17:44 -0500

    make config files .py files

---
+++ config.py
@@ -0,0 +1,13 @@
+#Configuration file, empty for user
+#Please enter the requested information in the line below the request.
+#Have each genome diff file on a separate line
+
+PROCEDURE = -1
+
+REFERENCE_GENOME = '''
+
+'''
+
+GENOME_DIFF_FILES = '''
+
+'''
