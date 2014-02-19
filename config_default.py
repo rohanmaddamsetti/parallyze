c2b46bb238781529cc64753ec65f88ee7480f3c2
Elizabeth Baird <eliz.baird@gmail.com>
2014-02-19 16:27:33 -0500

    add genomediff and refs and update default

--- config_default.py
+++ config_default.py
@@ -1,12 +1,12 @@
 #Configuration file default and test
 #Please enter the requested information in the line below the request
 
-PROCEDURE = 3
+PROCEDURE = 2
 REFERENCE_GENOME = '''
 ~/Users/elizabethbaird/Desktop/parallyze/reference.gb
 '''
 GENOME_DIFFS = '''
-~/Users/elizabethbaird/Desktop/parallyze/genomediff1.gd
-~/Users/elizabethbaird/Desktop/parallyze/genomediff2.gd
-~/Users/elizabethbaird/Desktop/parallyze/genomediff3.gd
+~/Users/elizabethbaird/Desktop/parallyze/Ara+3_REL10953.gd
+~/Users/elizabethbaird/Desktop/parallyze/Ara+3_REL10954.gd
+~/Users/elizabethbaird/Desktop/parallyze/Ara+6_REL10956.gd
 '''
