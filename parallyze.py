aef117cfc0f7392e6ba97f6c09cbd31585b2dec1
Elizabeth Baird <eliz.baird@gmail.com>
2014-02-19 11:28:07 -0500

    rehaul verification intro steps

--- parallyze.py
+++ parallyze.py
@@ -7,29 +7,49 @@
 #try to break
 
 import argparse
+import config
+import os
+import sys
+
+def file_list(fs):
+    flist = fs.split()
+    for f in flist:
+        assert os.path.isfile(f)
+    return flist
+
+def get_config():
+    conf = {}
+    if PROCEDURE == 1:
+        reference = REFERENCE_GENOME.strip()
+        assert os.path.isfile(reference)
+        assert reference.endswith('.gb')
+        conf['procedure'] = PROCEDURE
+        conf['reference'] = reference
+        return conf
+    elif PROCEDURE == 2:
+        reference = REFERENCE_GENOME.strip()
+        assert os.path.isfile(reference)
+        assert reference.endswith('.gb')
+        diffs = file_list(GENOME_DIFFS)
+        for diff_file in diffs:
+            assert diff_file.endswith('.gd')
+        conf['reference'] = reference
+        conf['procedure'] = PROCEDURE
+        conf['diffs'] = diffs
+        return conf
+    else:
+        print >>sys.stderr, 'Invalid procedure {p}! Exiting...'.format(p=PROCEDURE)
+        sys.exit()
 
 def main():
 	parser = argparse.ArgumentParser()
 #	parser.parse_args()
-	parser.add_argument(dest='config', help="First entry should be a config.txt file. File should be in working folder.")
+	parser.add_argument(dest='config', help="First entry should be a config.py file. File should be in working folder.")
 	parser.add_argument('-r', dest='reference', default="NONE")
-#	args.gdfiles=[]
- #       for i in args:
-  #              if i.endswith(".gd"):
-   #                     args.gdfiles.append(i)
-	args = parser.parse_args()
-	
-	print "\nHello World!\n"
-	if args.config != 'config.txt' or 'config_default.txt':
-		print "Error: please ensure file is titled config.txt"
-#	elif args.config != a text file in right format
-#		print "Error: please ensure file is a .txt file"
-#	elif args.config != contents of file properly formatted/layed out
-#		print "Error: please ensure contents of file are appropriate"
-	if args.reference:
-		print "The reference genome is", args.reference
-	print args
+    args = parse.parse_args()
 
-#	test if reference genome, procedure, and genomediff files are valid
-#	ensure, given each procedure, appropriate files are included
+    conf = get_config()
+    print conf['procedure'], conf['reference']
+
+
 main()
