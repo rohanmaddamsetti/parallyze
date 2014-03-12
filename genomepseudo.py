33845148339d4d3a6dc5036cd2673f391b033e23
Elizabeth Baird <eliz.baird@gmail.com>
2014-03-12 14:24:13 -0400

    create file for proc1 psuedocode, changes to parallyze?

---
+++ genomepseudo.py
@@ -0,0 +1,6 @@
+
+
+user inputs 1) reference genome and 2) genome diff files thru config file
+error check files
+(create list of dictionaries?) check/convert files to matrix (population/treatment/gene/generation/typemut/position/length)
+
