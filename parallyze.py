3570632725040cff4ce3a2d817d44799bd61b40f
Elizabeth Baird <eliz.baird@gmail.com>
2014-03-19 14:24:33 -0400

    maybe dealt with TypeError in conf[diffs] parsing

--- parallyze.py
+++ parallyze.py
@@ -7,6 +7,14 @@ import sys
 import config
 import config_default
 
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+from Bio.Seq import MutableSeq
+
+from numpy import random
+
+
 def file_list(fs):
     flist = fs.split()
     for f in flist:
@@ -18,7 +26,7 @@ def get_config():
     if config.PROCEDURE == '3':
         ref = config.REF_GENOME.strip()
         assert os.path.isfile(ref)
-        assert ref.endswith('.gb') 
+        assert ref.endswith('.gb' or '.gbk') 
         conf['procedure'] = config.PROCEDURE
 	conf['ref'] = ref
 	assert len(config.GENOME_DIFFS.strip())==0
@@ -26,7 +34,7 @@ def get_config():
     elif config.PROCEDURE in ['1','2','4','5']:  
         ref = config.REF_GENOME.strip()
         assert os.path.isfile(ref)
-        assert ref.endswith('.gb')
+        assert ref.endswith('.gb' or '.gbk')
         diffs = file_list(config.GENOME_DIFFS)
 	assert len(diffs)!=0
         for diff_file in diffs:   #annotated vs. non-annotated genomediff files
@@ -46,13 +54,6 @@ def get_config():
 get_config()
 
 #fp=open(filename, 'rU')
-
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.Alphabet import IUPAC
-from Bio.Seq import MutableSeq
-
-from numpy import random
 
 def base_to_int(i):
     if i == 'A':
@@ -75,51 +76,54 @@ def int_to_base(i):
         return 'T'
 
 def seq_to_int(seq):
-    converted=[base_to_int(b) for b in seq]
-    return converted
+    converted_to_int=[base_to_int(b) for b in seq]
+    return converted_to_int
 
 def int_to_seq(seq):
-    converted=[int_to_base(b) for b in seq]
-    return converted
+    converted_to_seq=[int_to_base(b) for b in seq]
+    return converted_to_seq
 
 def proc1(conf):
     mutlist=[]
     mutations={}
-    with open(conf['diffs']) as fp: #conf['diffs'] should be string or buffer?
-        for line in fp: 
-            if line.startswith('#' or 'JC' or 'RA' or 'UN'):
-                continue #does this start 'for line' loop again?
-            line=line.split()
-            mut_type=line[0]
-            mut_id=line[1]
-            parent_ids=line[2].split(',')
-            seq_id==line[3]
-            position==line[4]
-            if mut_type=='SNP':
-                new_seq==line[5]
-            elif mut_type=='SUB':
-                size==line[5]
-                new_seq==line[6]
-            elif mut_type=='DEL':
-                size==line[5]
-            elif mut_type=='INS':
-                new_seq==line[5]
-            elif mut_type=='MOB':
-                repeat_name==line[5]
-                strand==line[6]
-                duplication_size==line[7]
-            elif mut_type=='AMP':
-                size==line[5]
-                new_copy_number==line[6]
-            elif mut_type=='CON':
-                size==line[5]
-                region==line[6]
-            elif mut_type=='INV':
-                size==line[5]
-            #while mut_type count is 3 chars, continue - to terminate transfer of info when it gets gobbledy-gooky?
+    #loop over conf['diffs'] for each file
+    #fp=open(conf['diffs']) #maybe 'with' puts out nice error messages without it dying
+    for fname in conf['diffs']:
+        with open(fname) as fp: #conf['diffs'] should be string or buffer?
+            for line in fp: 
+                if line.startswith('#' or 'JC' or 'RA' or 'UN'):
+                    continue #does this start 'for line' loop again?
+                line=line.split()
+                mut_type=line[0]
+                mut_id=line[1]
+                parent_ids=line[2].split(',')
+                seq_id==line[3]
+                position==line[4]
+                if mut_type=='SNP':
+                    new_seq==line[5]
+                elif mut_type=='SUB':
+                    size==line[5]
+                    new_seq==line[6]
+                elif mut_type=='DEL':
+                    size==line[5]
+                elif mut_type=='INS':
+                    new_seq==line[5]
+                elif mut_type=='MOB':
+                    repeat_name==line[5]
+                    strand==line[6]
+                    duplication_size==line[7]
+                elif mut_type=='AMP':
+                    size==line[5]
+                    new_copy_number==line[6]
+                elif mut_type=='CON':
+                    size==line[5]
+                    region==line[6]
+                elif mut_type=='INV':
+                    size==line[5]
 
     #mutations[mut_id]={'type': mut_type, 'parents': parent_ids, ...)
-    #what is funciton of above line?
+    #what is function of above line?
+
 def proc3(conf):
     print '\n', 'Assumptions:', '\n', 'Synonymous mutations are neutral' '\n', 'Infinite sites model', '\n', 'Mutations are independent of one another', '\n', 'No defects to DNA repair', '\n', 'Mutation rate is constant across the genome', '\n', 'There is only one chromosome', '\n'
     lines=input("How many lines?  ")
