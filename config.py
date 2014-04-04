5395ab10e9159ac98c2890ce32d334c3b129079d
Elizabeth Baird <eliz.baird@gmail.com>
2014-04-04 16:11:49 -0400

    change absolute to relative paths

--- config.py
+++ config.py
@@ -6,11 +6,11 @@
 PROCEDURE = '1'
 
 REF_GENOME = '''
-/Users/Elizabeth/parallyze/reference.gbk
+reference.gbk
 '''
 
 GENOME_DIFFS = '''
-/Users/Elizabeth/parallyze/Ara-1_40K_10938_annotated.gd
-/Users/Elizabeth/parallyze/Ara-2_40K_11035_annotated.gd
-/Users/Elizabeth/parallyze/Ara-3_40K_10979_annotated.gd
+Ara-1_40K_10938_annotated.gd
+Ara-2_40K_11035_annotated.gd
+Ara-3_40K_10979_annotated.gd
 '''
