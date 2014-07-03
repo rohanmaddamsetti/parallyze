db720f5267ff97b8f4c6ee654507cb73229e860f
ejbaird <bairdel1@msu.edu>
2014-07-03 14:02:30 -0400

    insert gene_to_class code as comment

--- parallyze.py
+++ parallyze.py
@@ -134,6 +134,41 @@ def get_refseq(record):
     print
     return refseq
 
+     '''
+class Gene(object):
+
+    def __init__(self, my_start, my_end, gene_name, locus_tag, A, G, C, T):
+        self.my_start = my_start
+        self.my_end = my_end
+        self.gene_name = gene_name
+        self.locus_tag = locus_tag
+        self.A = 0
+        self.G = 0
+        self.C = 0
+        self.T = 0
+
+    def __str__(self):
+        return '{} ({}): ({},{}) A={} G={} C={} T={}'.format(self.gene_name, self.locus_tag, self.my_start, self.my_end, self.A, self.G, self.C, self.T)
+
+def get_genecoordinates(record):
+    geneinfo = {}
+    refseq = list(record.seq)
+    for i in record.features:
+        if i.type == 'CDS':
+            Gene.my_start = int(i.location.start)
+            Gene.my_end = int(i.location.end)
+            Gene.locus_tag = i.qualifiers['locus_tag'][0]
+            try:
+                Gene.gene_name = i.qualifiers['gene'][0]
+            except KeyError:
+                Gene.gene_name = 'none'
+            for base in refseq[Gene.my_start:Gene.my_end]:
+                Gene._ = Gene._.get(base,0) +  1
+            geneinfo.append(Gene)
+    print '\n', 'Reference genome list (1st 10): gene name, locus_tag, start, stop, A, G, C, T', '\n', geneinfo[:10]
+    return geneinfo
+     '''
+     
 def get_genecoordinates(record):
     '''input: record file from reference (i.e., ref as record file)
     get all IDed genes and their beginning and end position;
