3b39bc42e2bdebc7940cea0d0fed782a1aa42eba
Camille Scott <camille.scott.w@gmail.com>
2014-07-07 15:19:45 -0400

    Add base counting and feature parsing to Gene object, update get_coordinates

--- parallyze.py
+++ parallyze.py
@@ -135,46 +135,59 @@ def get_refseq(record):
     print
     return refseq
 
-     '''
+##
+
 class Gene(object):
 
-    def __init__(self, my_start, my_end, gene_name, locus_tag, A, G, C, T):
-        self.my_start = my_start
-        self.my_end = my_end
-        self.gene_name = gene_name
-        self.locus_tag = locus_tag
+    def __init__(self, feature):
+        assert feature.type == 'CDS'
+        self.locus_tag = feature.qualifiers['locus_tag'][0]
+        try:
+            self.name = feature.qualifiers['gene'][0]
+        except KeyError:
+            self.name = None
+        self.start = int(feature.location.start)
+        self.end = int(feature.location.end)
         self.A = 0
         self.G = 0
         self.C = 0
         self.T = 0
 
+    def count_bases(self, refseq):
+        for base in refseq[self.start:self.end]:
+            if base in ['A', 'a']:
+                self.A += 1
+            elif base in ['T', 't']:
+                self.T += 1
+            elif base in ['C', 'c']:
+                self.C += 1
+            elif base in ['G', 'g']:
+                self.G += 1
+            else:
+                pass
+
     def __str__(self):
-        return '{} ({}): ({},{}) A={} G={} C={} T={}'.format(self.gene_name, self.locus_tag, self.my_start, self.my_end, self.A, self.G, self.C, self.T)
+        return '{} ({}): ({},{}) A={} G={} C={} T={}'.format(self.name, self.locus_tag, 
+                                                            self.start, self.end, self.A, 
+                                                            self.G, self.C, self.T)
 
 def get_genecoordinates(record):
     geneinfo = {}
     refseq = list(record.seq)
-    for i in record.features:
-        if i.type == 'CDS':
-            Gene.my_start = int(i.location.start)
-            Gene.my_end = int(i.location.end)
-            Gene.locus_tag = i.qualifiers['locus_tag'][0]
-            try:
-                Gene.gene_name = i.qualifiers['gene'][0]
-            except KeyError:
-                Gene.gene_name = 'none'
-            for base in refseq[Gene.my_start:Gene.my_end]:
-                Gene._ = Gene._.get(base,0) +  1
-            geneinfo.append(Gene)
+    for feature in record.features:
+        if feature.type == 'CDS':
+            this_gene = Gene(feature)
+            this_gene.count_bases(refseq)
+            geneinfo[this_gene.locus_tag] = this_gene
     print '\n', 'Reference genome list (1st 10): gene name, locus_tag, start, stop, A, G, C, T', '\n', geneinfo[:10]
     return geneinfo
-            #jkjk see elstoich/line100
-     '''
-     
+
+##
+''' 
 def get_genecoordinates(record):
-    '''input: record file from reference (i.e., ref as record file)
-    get all IDed genes and their beginning and end position;
-    store in list of tuples'''
+    #input: record file from reference (i.e., ref as record file)
+    #get all IDed genes and their beginning and end position;
+    #store in list of tuples
     geneinfo = []
     for i in record.features:
         if i.type == 'CDS': #or i[type] if dictionary - tab everything below
@@ -190,7 +203,7 @@ def get_genecoordinates(record):
             geneinfo.append(mytuple)
     print '\n', 'Reference genome gene list (1st 10): start position, end pos, gene, locus_tag', '\n', geneinfo[:10]
     return geneinfo
-
+'''
 def str_keyvalue(data):
     s = '\n'.join([str(key)+': '+str(data[key]) for key in data])
     return s
