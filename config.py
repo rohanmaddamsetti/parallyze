b9e622c2ca8f0197de878050bc8377140f54d0bc
Camille Scott <camille.scott.w@gmail.com>
2014-07-09 22:22:15 -0400

    Include config.py from peasoup

--- config.py
+++ config.py
@@ -1,17 +1,70 @@
-#Configuration file, empty for user
-#Enter requested value(s) between quotations
-#Don't delete quote marks
-#To run default data, leave all spaces empty
-
-PROCEDURE ='6'
-
-REF_GENOME = '''
-REL606.2.gbk
+#!/usr/bin/env python
 '''
 
-GENOME_DIFFS = '''
-Ara+1_50K_11392_annotated.gd  
-Ara+1_50K_11393_annotated.gd  
-Ara+2_50K_11342_annotated.gd  
-Ara+2_50K_11343_annotated.gd  
+Part of parallyze
+by Elizabeth Baird
+
+this file originates in peasoup
+by Camille Scott
+@ www.github.com/camillescott/peasoup
+
+Defines a simple config file parsing class.
+
 '''
+
+def coroutine(func):
+    def start(*args,**kwargs):
+        cr = func(*args,**kwargs)
+        cr.next()
+        return cr
+    return start
+
+class SimpleConfig(object):
+
+    def __init__(self, key_types, fn):
+        self.key_types = key_types
+        self.fn = fn
+        self.parse()
+    
+    def parse(self):
+        
+        parser = SimpleConfig.parse_protocol(
+                key_types=self.key_types, target=self.parser_receiver())
+        with open(self.fn, 'rb') as fp:
+            for line in fp:
+                for symbol in line.strip().split():
+                    parser.send(symbol.strip())
+            parser.send(None)
+
+    @staticmethod
+    @coroutine
+    def parse_protocol(key_types=None, target=None):
+
+        key = None
+        value = []
+
+        while True:
+            symbol = (yield)
+            if symbol in key_types or symbol == None:
+                if value:
+                    target.send((key,value))
+                    value = []
+                key = symbol
+            elif key:
+                value.append(symbol)
+    
+    @coroutine
+    def parser_receiver(self):
+
+        while True:
+            key, value = (yield)
+            value = [self.key_types[key](v) for v in value]
+            if len(value) == 1:
+                value = value[0]
+
+            print '{k} : {v}'.format(k=key, v='\n'.join(value) \
+                                    if type(value)==list else value)
+            
+            setattr(self, key, value)
+            
+            
